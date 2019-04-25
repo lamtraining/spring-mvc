be647ed7d41e82d60dace6eeb50511822557cbcd
unknown <lamjavadev456@gmail.com>
2019-04-26 01:11:17 +0700

    done

---
+++ src/main/java/com/laptrinhjavaweb/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.laptrinhjavaweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/trang-chu", method = RequestMethod.GET)
+	public ModelAndView homePage() {
+		ModelAndView mav = new ModelAndView("home");
+		return mav;
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Home Page</title>
+
+</head>
+
+<body>
+
+<h1>Hello World</h1>
+
+</body>
+
+</html>
