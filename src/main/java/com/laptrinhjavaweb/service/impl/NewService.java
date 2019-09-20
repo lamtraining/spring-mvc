30500a72cdc17a28c1a7063b950449c83dee5428
unknown <lamjavadev456@gmail.com>
2019-09-20 09:18:39 +0700

    part 1

--- src/main/java/com/laptrinhjavaweb/service/impl/NewService.java
+++ src/main/java/com/laptrinhjavaweb/service/impl/NewService.java
@@ -1,22 +1,30 @@
 package com.laptrinhjavaweb.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.laptrinhjavaweb.dao.INewDAO;
+import com.laptrinhjavaweb.entity.NewEntity;
 import com.laptrinhjavaweb.model.NewModel;
+import com.laptrinhjavaweb.repository.NewRepository;
 import com.laptrinhjavaweb.service.INewService;
 
 @Service
 public class NewService implements INewService {
 	
 	@Autowired
-	private INewDAO newDao;
+	private NewRepository newRepository;
 	
 	@Override
 	public List<NewModel> findAll() {
-		return newDao.findAll();
+		List<NewModel> models = new ArrayList<>();
+		List<NewEntity> entities = newRepository.findAll();
+		for (NewEntity item: entities) {
+			NewModel newModel = new NewModel();
+			newModel.setTitle(item.getTitle());
+			newModel.setShortDescription(item.getShortDescription());
+			models.add(newModel);
+		}
+		return models;
 	}
 }
